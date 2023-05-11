c917421e85a370c415313f3c20e75bbcf4f370db
m7mdhani <Mohamed.Hani@applogica.com>
2023-05-11 16:05:46 +0300

    button-package

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import ApplogicaButton from "./components/ApplogicaButton/ApplogicaButton";
 import Sidebar from "./components/Sidebar/Sidebar";
 import CardContainer from "./components/CardContainer/CardContainer";
 
+import { Button } from "poc-react";
+
 import "./App.css";
 
 function App() {
@@ -10,6 +12,11 @@ function App() {
       <ApplogicaButton>Save</ApplogicaButton>
       <Sidebar />
       <CardContainer />
+      <Button
+        style={{ backgroundColor: "#eeee", border: "0", padding: "10px" }}
+      >
+        Hello from Local button package
+      </Button>
     </div>
   );
 }
